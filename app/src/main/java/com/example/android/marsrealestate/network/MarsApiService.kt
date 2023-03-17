27e1747fe31463f0a635ab0f642951c4deda8f9a
unknown <nahuelji.773@gmail.com>
2023-03-17 20:15:49 -0300

    Changed ScalarsConverter to MoshiConverter. Data class added

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,22 +17,41 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+//import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
 //Retrofit builder with ConverterFactory and BASE_URL
-
-private val retrofit = Retrofit.Builder()
+/*private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addConverterFactory(ScalarsConverterFactory.create())
     .build()
+  */
 
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
 
 
 /**
@@ -40,9 +59,15 @@ Interface that explains how Retrofit talks to the web server using HTTP requests
 Retrofit will create an object that implements the interface with all of the methods that talk to the server
 Conceptually similar the way Room implements DAO's*/
 
-interface MarsApiService {
+/*interface MarsApiService {
     @GET("realestate")                  //Annotating the method with @GET specifies the endpoint for the JSON real estate response
     fun getProperties(): Call<String>   //Retrofit Call object that will start the HTTP request.
+}*/
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<List<MarsProperty>>
 }
 
 /**
